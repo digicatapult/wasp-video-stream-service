89e11d6a9c28cbfe06234f32d63479e23b3426a1
Darryl Morton <darryl.morton@digicatapult.org.uk>
2022-03-30 11:41:23 +0100

    removing kafka config max size, default 1MB is enough for 640x480 streaming

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ const queue = [];
 
 const video = document.querySelector('video');
 video.src = window.URL.createObjectURL(mediaSource);
+// video.play();
 
 const mimeCodec = 'video/mp4; codecs="avc1.64001e, mp4a.40.2"';
 
 let connected = false;
+let segmentIndex = 0;
 
 const connect = function() {
-    // let socketStartTime = performance.now();
     if (!connected) {
         websocket = new WebSocket('ws://localhost:9999/ws');
         websocket.binaryType = 'arraybuffer';
@@ -22,22 +23,10 @@ const connect = function() {
             console.log("Connected to the web socket");
         };
         websocket.onmessage = function (e) {
-            // let blob = m.data;
-            // renderImage(dCtx, blob);
-            // const now = performance.now();
-            // socketStartTime = now;
-
-            console.log('websocket message e.data: ' + e.data);
-
             if (typeof e.data !== 'string') {
-                console.log("typeof e.data !== 'string'", e.data);
-
-                if (buffer.updating || queue.length > 0) {
-                    console.log("buffer.updating || queue.length > 0");
-
+                if (buffer.updating) {
                     queue.push(e.data);
                 } else {
-                    console.log("ELSE");
                     buffer.appendBuffer(e.data);
                 }
             }
@@ -49,21 +38,27 @@ mediaSource.addEventListener('sourceopen', function(e) {
     console.log('media source sourceopen');
 
     connect()
-    // video.play();
 
     buffer = mediaSource.addSourceBuffer(mimeCodec);
 
-    buffer.addEventListener('updatestart', function(e) { console.log('updatestart: ' + mediaSource.readyState); });
-    buffer.addEventListener('updateend', function(e) { console.log('updateend: ' + mediaSource.readyState); });
+    buffer.addEventListener('updatestart', function(e) {
+        console.log('updatestart: ' + mediaSource.readyState);
+
+        segmentIndex++;
+    });
+    buffer.addEventListener('updateend', function(e) {
+        console.log('updateend: ' + mediaSource.readyState);
+
+        video.play();
+    });
     buffer.addEventListener('error', function(e) { console.log('error: ' + mediaSource.readyState); });
     buffer.addEventListener('abort', function(e) { console.log('abort: ' + mediaSource.readyState); });
 
     buffer.addEventListener('update', function() {
         console.log('buffer update: ' + mediaSource.readyState);
+        console.log('buffer update segmentIndex: ', segmentIndex);
 
-        if (queue.length > 0 && !buffer.updating) {
-            buffer.appendBuffer(queue.shift());
-        }
+        buffer.appendBuffer(queue.shift());
     });
 
     mediaSource.addEventListener('sourceended', function(e) { console.log('sourceended: ' + mediaSource.readyState); });
